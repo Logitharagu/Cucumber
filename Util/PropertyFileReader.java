2a2d4f80c24d2c6b6f821761370582edf8bef6be
Logitharagu <131980796+Logitharagu@users.noreply.github.com>
2025-07-07 23:44:05 +0530

    Add files via upload

---
+++ Util/PropertyFileReader.java
@@ -0,0 +1,29 @@
+package Util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyFileReader {
+		
+		public static String readDataFromPropertyFile(String fileName,String propertyKey) {
+			File file = new File("./data/"+fileName+".properties");
+			FileInputStream fis;
+			String propertyValue = "";
+			try {
+				fis = new FileInputStream(file);
+				Properties properties = new Properties();
+				properties.load(fis);
+				propertyValue = properties.getProperty(propertyKey);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return propertyValue;
+		
+		
+		
+	}
+
+}
